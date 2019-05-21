ead9f0288493509ab1b52cd8074e99754576cb1a
Rifka Annisa <Geminigirl124@gmail.com>
2019-05-21 19:46:41 +0700

    memulai dari 0

--- controller/gatesystem.js
+++
@@ -1,9 +0,0 @@
-var db = require("../config");
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(request, response) {
-  response.render('gatesystem.tl');
-});
-
-module.exports = router;
--- server.js
+++
@@ -1,7 +0,0 @@
-var app = require("./app");
-
-var PORT = process.env.PORT || 9999;
-
-app.listen(PORT, '0.0.0.0', function() {
-    console.log('Listening to port:  ' + PORT);
-});
