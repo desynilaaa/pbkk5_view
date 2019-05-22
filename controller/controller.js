5924d144028e01013f7b285648746e4e4e37e4ab
Rifka Annisa <Geminigirl124@gmail.com>
2019-05-22 08:27:55 +0700

    add login dan register

--- controller/controller.js
+++ controller/controller.js
@@ -6,7 +6,8 @@ var dbconfig = require('../config/database');
 var mysql = require('mysql');
 var connection = mysql.createConnection(dbconfig.connection);
 var bcrypt = require('bcrypt-nodejs');
-
+var result = {};
+result["message"] = "";
 
 var url = " http://715f9452.ngrok.io/";
 
@@ -23,9 +24,46 @@ exports.getRegister= (req,res)=>{
 };
 
 exports.doLogin= (req,res)=>{
+
+  	global.nrp = req.body.nrp;
+  	global.password = req.body.password;
+  	global.id_gate = req.body.id_gate;
+    console.log(global.nrp);
+    console.log(global.password);
+    console.log(global.id_gate);
+  	Request.post({
+  		url:     url+'login/auth',
+  		form:    { username: global.nrp, password: global.password, id_gate_log: global.id_gate }
+  	  }, function(error, response, body){
+        // console.log(body);
+      // console.dir(JSON.parse(body));
+      result = JSON.parse(body);
+  		console.dir(result["message"]);
+  		if(result["message"] === "gagal" || result["message"] === "Tolong masukkan NRP dan Password!" || result["message"]=== "Incorrect NRP and/or Password!" ){
+  			  res.redirect('/login');
+  		}else if(result["message"] === "berhasil"){
+  				res.redirect('/');
+        }else{
+          res.redirect('/login');
+        }
+  	  });
 };
 
 exports.getLogin= (req,res)=>{
+  global.gate;
+  Request.get(url+"gates", (error, response, body) => {
+      if(error) {
+          return console.dir(error);
+      }
+      console.dir(JSON.parse(body));
+      global.gate = JSON.parse(body);
+      gate = global.gate;
+      res.render('login',{
+        message : result["message"],
+        rows:gate
+      });
+  });
+
 };
 
 // exports.getLogout= (req,res)=>{
